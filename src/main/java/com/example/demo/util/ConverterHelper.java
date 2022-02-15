d74e0f74fd6049b2f0c1769ac4a271e1b88c27b9
Septian Solihin <septian.solihin@btpn.com>
2022-02-15 14:05:05 +0700

    refactor code to add static initialization for new instance model mapper

--- src/main/java/com/example/demo/util/ConverterHelper.java
+++ src/main/java/com/example/demo/util/ConverterHelper.java
@@ -3,9 +3,17 @@ package com.example.demo.util;
 import org.modelmapper.ModelMapper;
 
 public class ConverterHelper {
-    private static final ModelMapper modelMapper = new ModelMapper();
+    private static final ModelMapper modelMapper;
     
     private ConverterHelper(){}
+    
+    static {
+        try{
+            modelMapper = new ModelMapper();
+        }catch (Exception e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
     
     public static <T, V> T convertDtoToEntity(V dtoObject, Class<T> targetClass){
         return modelMapper.map(dtoObject, targetClass);
