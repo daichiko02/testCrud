ecc9d9b9b78f8d66457c650254f7e977f8509ea1
Septian Solihin <septian.solihin@btpn.com>
2022-02-15 12:48:12 +0700

    add user entity and user DTO

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -1,8 +1,39 @@
 package com.example.demo.user;
 
-import javax.persistence.Entity;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity( name = "customer")
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity( name = "user")
 public class User {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Setter
+    private String username;
+    
+    @Setter
+    private String name;
+    
+    @Setter
+    private String password;
+    
+    @Setter
+    private String email;
+    
+    @CreationTimestamp
+    @EqualsAndHashCode.Exclude
+    private OffsetDateTime createdAt;
+    
+    @UpdateTimestamp
+    @EqualsAndHashCode.Exclude
+    private OffsetDateTime modifiedAt;
 }
---
+++ src/main/java/com/example/demo/user/UserRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.demo.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDto{
+    @NotNull
+    private String username;
+    
+    @NotNull
+    private String password;
+    
+    @NotNull
+    private String name;
+    
+    @NotNull
+    private String email;
+}
